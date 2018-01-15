hroptatyr/phashist

initial import of toolchain
Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

src/version.h
@@ -1,0 +1,7 @@
+#ifndef INCLUDED_version_h_
+#define INCLUDED_version_h_
+
+extern const char echse_version_string[];
+#define package_version	echse_version_string
+
+#endif	/* INCLUDED_version_h_ */

hroptatyr/dtcl

src/version.h
@@ -1,0 +1,7 @@
+#ifndef INCLUDED_version_h_
+#define INCLUDED_version_h_
+
+extern const char _version_string[];
+#define package_version	_version_string
+
+#endif	/* INCLUDED_version_h_ */