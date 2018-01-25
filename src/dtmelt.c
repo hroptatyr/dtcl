b3244c6b2a6bbf4bbbc4c2aff3373334ab924b93
Sebastian Freundt <freundt@ga-group.nl>
2018-01-25 10:14:28 +0000

    minor, use FORMULA for dtmelt(1) too

--- src/dtmelt.c
+++ src/dtmelt.c
@@ -46,12 +46,20 @@
 #include "nifty.h"
 
 static int hdrp = 0;
-/* idvars (we call them dimensions) */
-static size_t ndims;
-static size_t *dims;
-/* measure vars */
-static size_t nmeas;
-static size_t *meas;
+static int cnmp = 0;
+
+/* idvars (our left hand side) */
+static size_t nlhs;
+static union {
+	size_t v;
+	size_t *p;
+} lhs = {-1};
+/* measure vars (our right hand side) */
+static size_t nrhs;
+static union {
+	size_t v;
+	size_t *p;
+} rhs = {-1};
 
 
 static void
@@ -71,33 +79,59 @@ error(const char *fmt, ...)
 	return;
 }
 
+static inline const char*
+memchrnul(const char *s, int c, size_t z)
+{
+	return memchr(s, c, z) ?: s + z;
+}
+
+
 static int
 chck(size_t ncol)
 {
-	for (size_t i = 0U; i < ndims; i++) {
-		if (UNLIKELY(dims[i] >= ncol)) {
+	if (!nlhs) {
+		if (UNLIKELY(lhs.v >= ncol && lhs.v + 1U)) {
+			return -1;
+		}
+	} else for (size_t i = 0U; i < nlhs; i++) {
+		if (UNLIKELY(lhs.p[i] >= ncol)) {
 			return -1;
 		}
 	}
-	for (size_t i = 0U; i < nmeas; i++) {
-		if (UNLIKELY(meas[i] >= ncol)) {
+	if (!nrhs) {
+		if (UNLIKELY(rhs.v >= ncol && rhs.v + 1U)) {
+			return -1;
+		}
+	} else for (size_t i = 0U; i < nrhs; i++) {
+		if (UNLIKELY(rhs.p[i] >= ncol)) {
 			return -1;
 		}
 	}
-	if (!nmeas) {
+	if (UNLIKELY(!nlhs && !nrhs && !(lhs.v + 1U) && !(rhs.v + 1U))) {
+		return -1;
+	}
+
+	if (!nrhs && !(rhs.v + 1U)) {
 		/* construct the set of measure vars */
-		if (UNLIKELY(!(meas = calloc(ncol, sizeof(*meas))))) {
+		nrhs = ncol - nlhs - 1 + (nlhs > 0);
+		if (UNLIKELY(!(rhs.p = calloc(nrhs, sizeof(*rhs.p))))) {
 			return -1;
-		}
-		for (size_t i = 0U; i < ncol; i++) {
-			size_t j;
-			for (j = 0U; j < ndims; j++) {
-				if (i == dims[j]) {
-					break;
+		} else if (!nlhs) {
+			for (size_t i = 0U, j = 0U; i < ncol; i++) {
+				if (i == lhs.v) {
+					continue;
+				}
+				rhs.p[j++] = i;
+			}
+		} else for (size_t i = 0U, j = 0U; j < nrhs; i++) {
+			for (size_t k = 0U; k < nlhs; k++) {
+				if (i == lhs.p[k]) {
+					goto found;
 				}
 			}
-			meas[nmeas] = i;
-			nmeas += j >= ndims;
+			rhs.p[j++] = i;
+		found:
+			continue;
 		}
 	}
 	return 0;
@@ -135,6 +169,108 @@ tokln1(size_t *restrict c, size_t nc, const char *ln, size_t lz)
 	return j;
 }
 
+static ssize_t
+find_s(const char *ss, const size_t *of, size_t nc, const char *s, size_t z)
+{
+/* find S of size Z in {SS + OF} */
+	for (size_t i = 0U; i < nc; i++) {
+		const size_t bo = of[i + 0U];
+		const size_t eo = of[i + 1U];
+		if (z == eo - bo - 1U &&
+		    !memcmp(ss + bo, s, z)) {
+			return i;
+		}
+	}
+	/* not found */
+	return -1;
+}
+
+static int
+snrf(const char *formula, const char *hn, const size_t *of, size_t nc)
+{
+	static const char *form;
+	const char *elhs, *l;
+	const char *erhs, *r;
+	const char *on;
+	size_t nl, nr;
+
+	if (UNLIKELY((formula = form ?: formula) == NULL)) {
+		return !form - 1;
+	}
+	if ((elhs = strchr(l = form = formula, '~')) == NULL) {
+		return -1;
+	}
+	r = elhs + 1U;
+	erhs = r + strlen(r);
+
+	for (nl = 0U, on = l; (on = memchr(on, '+', elhs - on)); nl++, on++);
+	for (nr = 0U, on = r; (on = memchr(on, '+', erhs - on)); nr++, on++);
+
+	if (nl && !(lhs.v + 1U)) {
+		lhs.p = calloc(nlhs = nl + 1U, sizeof(*lhs.p));
+	}
+	if (nr && !(rhs.v + 1U)) {
+		rhs.p = calloc(nrhs = nr + 1U, sizeof(*rhs.p));
+	}
+
+	/* now try and snarf the whole shebang */
+	if (!nl) {
+		goto one_l;
+	}
+	for (nl = 0U; nl < nlhs; nl++, l = on + 1U) {
+		/* try with numbers first */
+		char *tmp;
+		long unsigned int x;
+
+	one_l:
+		on = memchrnul(l, '+', elhs - l);
+		if ((x = strtoul(l, &tmp, 10)) && tmp == on) {
+			x--;
+		} else if ((x = find_s(hn, of, nc, l, on - l)) < nc) {
+			;
+		} else {
+			return -1;
+		}
+
+		if (!nlhs) {
+			lhs.v = x;
+		} else {
+			lhs.p[nl] = x;
+		}
+	}
+
+	/* snarf right hand side */
+	if (!nr) {
+		goto one_r;
+	}
+	for (nr = 0U; nr < nrhs; nr++, r = on + 1U) {
+		/* try with numbers first */
+		char *tmp;
+		long unsigned int x;
+
+	one_r:
+		on = memchrnul(r, '+', erhs - r);
+		if ((x = strtoul(r, &tmp, 10)) && tmp == on ||
+		    tmp + 3U == on && !nrhs &&
+		    tmp[0U] == '.' && tmp[1U] == '.' && tmp[2U] == '.') {
+			x--;
+		} else if ((x = find_s(hn, of, nc, r, on - r)) < nc) {
+			;
+		} else {
+			return -1;
+		}
+
+		if (!nrhs) {
+			rhs.v = x;
+		} else {
+			rhs.p[nr] = x;
+		}
+	}
+	/* all is good, forget about the formula then */
+	form = NULL;
+	return 0;
+}
+
 static int
 proc1(void)
 {
@@ -146,10 +282,13 @@ proc1(void)
 	size_t *coff;
 	/* line number */
 	size_t nr = 0U;
+	/* offsets for header and header buffer */
+	char *hn = NULL;
+	size_t *hoff = NULL;
 	/* constant dimension line */
-	size_t ndimln = 0U;
-	size_t zdimln = 0U;
-	char *dimln = NULL;
+	size_t ndln = 0U;
+	size_t zdln = 0U;
+	char *dln = NULL;
 
 	/* probe */
 	if (UNLIKELY((nrd = getline(&line, &llen, stdin)) < 0)) {
@@ -179,8 +318,33 @@ Error: cannot allocate memory to hold one line");
 	if (!hdrp) {
 		goto tok;
 	}
+	/* otherwise snarf col names as defined in header */
+	if (UNLIKELY((hn = strndup(line, nrd)) == NULL ||
+		     (hoff = calloc(ncol + 1U, sizeof(*hoff))) == NULL)) {
+		error("\
+Error: cannot allocate memory to hold a copy of the header");
+		rc = -1;
+		goto err;
+	}
+	tokln1(hoff, ncol, line, nrd);
+	for (size_t i = 1U; i <= ncol; i++) {
+		hn[hoff[i] - 1U] = '\0';
+	}
+	/* we might need to rescan the formula now */
+	if (UNLIKELY(snrf(NULL, hn, hoff, ncol)) < 0) {
+		error("\
+Error: cannot interpret formula");
+		rc = -1;
+		goto err;
+	} else if (UNLIKELY(chck(ncol) < 0)) {
+		errno = 0, error("\
+Error: fewer columns present than needed for LHS~RHS and value");
+		rc = -1;
+		goto err;
+	}
 
 	while ((nrd = getline(&line, &llen, stdin)) > 0) {
+		size_t bo, eo, v;
 	tok:
 		nr++;
 		size_t nf = tokln1(coff, ncol, line, nrd);
@@ -193,60 +357,50 @@ Error: line %zu has only %zu columns, expected %zu", nr, nf, ncol);
 		}
 
 		/* construct constant dimension prefix */
-		ndimln = 0U;
-		for (size_t i = 0U; i < ndims; i++) {
-			const size_t of = coff[dims[i] + 0U];
-			const size_t eo = coff[dims[i] + 1U];
-
-			if (UNLIKELY(ndimln + eo - of > zdimln)) {
+		ndln = 0U;
+		if (!nlhs) {
+			bo = coff[lhs.v + 0U];
+			eo = coff[lhs.v + 1U];
+			goto one_l;
+		} else for (size_t i = 0U; i < nlhs; i++) {
+			bo = coff[lhs.p[i] + 0U];
+			eo = coff[lhs.p[i] + 1U];
+		one_l:
+			if (UNLIKELY(ndln + eo - bo > zdln)) {
 				/* resize */
-				while ((zdimln = (zdimln * 2U) ?: 256U) <=
-				       ndimln + eo - of);
-				dimln = realloc(dimln, zdimln * sizeof(*dimln));
+				while ((zdln = (zdln * 2U) ?: 256U) <=
+				       ndln + eo - bo);
+				dln = realloc(dln, zdln * sizeof(*dln));
 			}
-			memcpy(dimln + ndimln, line + of, eo - of - 1);
-			dimln[(ndimln += eo - of) - 1] = '\t';
+			memcpy(dln + ndln, line + bo, eo - bo - 1);
+			ndln += eo - bo - 1;
+			dln[ndln++] = '\t';
 		}
 
-		for (size_t i = 0U; i < nmeas; i++) {
-			const size_t of = coff[meas[i] + 0U];
-			const size_t eo = coff[meas[i] + 1U];
-
-			fwrite(dimln, sizeof(*dimln), ndimln, stdout);
+		if (!nrhs) {
+			bo = coff[rhs.v + 0U];
+			eo = coff[rhs.v + 1U];
+			v = rhs.v;
+			goto one_r;
+		} else for (size_t i = 0U; i < nrhs; i++) {
+			bo = coff[rhs.p[i] + 0U];
+			eo = coff[rhs.p[i] + 1U];
+			v = rhs.p[i];
+		one_r:
+			fwrite(dln, sizeof(*dln), ndln, stdout);
 			/* header or index */
-			fprintf(stdout, "%zu", meas[i] + 1U);
+			fprintf(stdout, "V%zu", v + 1U);
 			fputc('\t', stdout);
-			fwrite(line + of, sizeof(*line), eo - of - 1, stdout);
+			fwrite(line + bo, sizeof(*line), eo - bo - 1, stdout);
 			fputc('\n', stdout);
 		}
 	}
+err:
 	free(coff);
-	free(dimln);
+	free(dln);
 out:
 	free(line);
 	return rc;
-}
-
-static ssize_t
-rdvars(size_t *restrict *tgt, const char *const *v, size_t n)
-{
-	size_t m = 0U;
-
-	if (UNLIKELY(!n)) {
-		return 0U;
-	} else if (UNLIKELY((*tgt = calloc(n, sizeof(**tgt))) == NULL)) {
-		error("\
-Error: not enough memory.");
-		return -1;
-	}
-	for (size_t i = 0U; i < n; i++) {
-		char *on;
-		if (((*tgt)[m] = strtoul(v[i], &on, 10))) {
-			(*tgt)[m]--;
-			m++;
-		}
-	}
-	return m;
 }
 
 
@@ -263,18 +417,28 @@ main(int argc, char *argv[])
 		goto out;
 	}
 
-	with (ssize_t nd = rdvars(&dims, argi->idvar_args, argi->idvar_nargs)) {
-		if (UNLIKELY(nd < 0)) {
-			rc = 1;
-			goto out;
-		}
-		ndims = nd;
+	/* overread and/or expect headers? */
+	hdrp = argi->header_flag;
+	/* memorise that we want col names for STCC() later on */
+	cnmp = argi->col_names_flag;
+
+	/* snarf formula */
+	if (UNLIKELY(!argi->nargs ||
+		     snrf(*argi->args, NULL, NULL, 0U) < 0 && !hdrp)) {
+		error("\
+Error: cannot interpret formula");
+		rc = 1;
+		goto out;
 	}
 
 	rc = proc1() < 0;
 
-	free(dims);
-	free(meas);
+	if (nlhs) {
+		free(lhs.p);
+	}
+	if (nrhs) {
+		free(rhs.p);
+	}
 out:
 	yuck_free(argi);
 	return rc;
